60ab87c50c0e5234ee1afe8fa9739abeb40aa67a
Stef-00012 <stefano.delprete.08@gmail.com>
2024-03-19 14:02:32 +0100

    create commands on first run

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,21 @@
 const axios = require('axios')
 
-module.exports = (client, int) => {
+module.exports = async (client, int) => {
     console.log("The app is online")
+    
+    const commands = await client.application.commands.fetch()
+    
+    if (commands.length == 0) {
+        try {
+            await axios.put(`https://discord.com/api/v10/applications/${client.user.id}/commands`, commands, {
+                headers: {
+                    Authorization: `Bot ${client.config.token}`,
+                    'Content-Type': 'application/json; charset=UTF-8',
+                    'User-Agent': 'DiscordBot (discord.js, 14.14.1 (modified))'
+                }
+            })
+        } catch(err) {
+            console.error(JSON.stringify(err.response.data, null, 2))
+        }
+    }
 }
