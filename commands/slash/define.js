fe26cbaea97ee014c344b992fe68c711467b1404
Stef-00012 <stefano.delprete.08@gmail.com>
2024-07-08 21:48:15 +0200

    edit define command

--- commands/slash/define.js
+++ commands/slash/define.js
@@ -21,7 +21,7 @@ module.exports = {
     		    try {
     		        const wordDefinition = await ubdict.define(word)
     		    
-        		    return wordDefinition[0].permalink
+        		    return `[${word}](${wordDefinition[0].permalink})`
     		    } catch(e) {
     		        return word
     		    }
@@ -31,7 +31,7 @@ module.exports = {
     		    try {
     		        const wordDefinition = await ubdict.define(word)
     		    
-        		    return wordDefinition[0].permalink
+        		    return `[${word}](${wordDefinition[0].permalink})`
     		    } catch(e) {
     		        return word
     		    }
