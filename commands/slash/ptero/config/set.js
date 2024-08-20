239791c7195cea0bb7726ad7257a21871107e28a
Stef-00012 <stefano.delprete.08@gmail.com>
2024-08-20 20:36:10 +0200

    remove path from panelUrl

--- commands/slash/ptero/config/set.js
+++ commands/slash/ptero/config/set.js
@@ -1,11 +1,14 @@
 module.exports = async (client, int) => {
-    const panelUrl = int.options.getString('url')
+    let panelUrl = int.options.getString('url')
     const apiKey = int.options.getString('key')
 
     if (!panelUrl.startsWith('http://') && !panelUrl.startsWith('https://')) return int.reply({
         content: "The provided panel URL is invalid",
         ephemeral: true
     })
+
+    panelUrl = panelUrl.split('/').slice(0, 3).join('/')
+
 
     if (!apiKey.startsWith('ptlc_')) return int.reply({
         content: "Invalid API key",
