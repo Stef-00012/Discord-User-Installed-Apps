f2b93c78af493f9fdae1a7796282fa1a04e79ec6
Stef-00012 <stefano.delprete.08@gmail.com>
2024-10-04 10:43:17 +0200

    ip command [untested]

--- src/commands/slash/ip.js
+++ src/commands/slash/ip.js
@@ -21,21 +21,21 @@ module.exports = {
 		const embed = new EmbedBuilder();
 
 		const fields = [
-    { name: "Country", value: ipData.country, inline: true },
-    { name: "Country Code", value: ipData.countryCode, inline: true },
-    { name: "Region", value: ipData.region }, // Not inline, so it appears on a new line
-    { name: "Region Name", value: ipData.regionName }, // Not inline, so it appears on a new line
-    { name: "City", value: ipData.city, inline: true },
-    { name: "ZIP", value: ipData.zip, inline: true }, // Inline with City
-    { name: "Latitude", value: `${ipData.lat}`, inline: true },
-    { name: "Longitude", value: `${ipData.lon}`, inline: true }, // Inline with Latitude
-    { name: "Timezone", value: ipData.timezone }, // Not inline, so it appears on a new line
-    { name: "ISP", value: ipData.isp, inline: true },
-    { name: "ORG", value: ipData.org, inline: true },
-    { name: "AS", value: ipData.as } // Not inline, so it appears on a new line
-];
-
-embed.set
+            { name: "Country", value: ipData.country, inline: true },
+            { name: "Country Code", value: ipData.countryCode, inline: true },
+            { name: "Region", value: ipData.region },
+            { name: "Region Name", value: ipData.regionName, inline: true },
+            { name: "City", value: ipData.city },
+            { name: "ZIP", value: ipData.zip, inline: true },
+            { name: "Latitude", value: `${ipData.lat}` },
+            { name: "Longitude", value: `${ipData.lon}`, inline: true },
+            { name: "Timezone", value: ipData.timezone },
+            { name: "ISP", value: ipData.isp, inline: true },
+            { name: "ORG", value: ipData.org, inline: true },
+            { name: "AS", value: ipData.as, inline: true }
+        ];
+        
+        embed.setFields('field')
 		
 		console.log(fields)
 
