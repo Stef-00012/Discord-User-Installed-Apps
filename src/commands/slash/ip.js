640f03f0d486f399ea2d704aabeede9d3f7e1c3d
Stef-00012 <stefano.delprete.08@gmail.com>
2024-10-04 10:38:25 +0200

    ip command [untested]

--- src/commands/slash/ip.js
+++ src/commands/slash/ip.js
@@ -21,63 +21,21 @@ module.exports = {
 		const embed = new EmbedBuilder();
 
 		const fields = [
-			{
-				name: "Country",
-				value: ipData.country,
-				inline: true
-			},
-			{
-				name: "Country Code",
-				value: ipData.countryCode,
-			},
-			{
-				name: "Region",
-				value: ipData.region,
-				inline: true
-			},
-			{
-				name: "Region Name",
-				value: ipData.regionName,
-			},
-			{
-				name: "City",
-				value: ipData.city,
-				inline: true
-			},
-			{
-				name: "ZIP",
-				value: ipData.zip,
-			},
-			{
-				name: "Latitute",
-				value: `${ipData.lat}`,
-				inline: true
-			},
-			{
-				name: "Longitude",
-				value: `${ipData.lon}`,
-			},
-			{
-				name: "Timezone",
-				value: ipData.timezone,
-			},
-			{
-				name: "ISP",
-				value: ipData.isp,
-				inline: true
-			},
-			{
-				name: "ORG",
-				value: ipData.org,
-				inline: true
-			},
-			{
-				name: "AS",
-				value: ipData.as,
-			},
-		];
-		
-		embed.setFields(fields)
+    { name: "Country", value: ipData.country, inline: true },
+    { name: "Country Code", value: ipData.countryCode, inline: true },
+    { name: "Region", value: ipData.region, inline: true },
+    { name: "Region Name", value: ipData.regionName },
+    { name: "City", value: ipData.city, inline: true },
+    { name: "ZIP", value: ipData.zip },
+    { name: "Latitude", value: `${ipData.lat}`, inline: true },
+    { name: "Longitude", value: `${ipData.lon}` },
+    { name: "Timezone", value: ipData.timezone },
+    { name: "ISP", value: ipData.isp, inline: true },
+    { name: "ORG", value: ipData.org, inline: true },
+    { name: "AS", value: ipData.as }
+];
+
+embed.setFields(fields);
 		
 		console.log(fields)
 
